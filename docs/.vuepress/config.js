cadf10e6ea692672314eb2fbc887f68e2864992a
20012477 <gaomingxi@haier.com>
2020-07-29 11:00:21 +0800

    add

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'VuePress 在21云盒子',
+  description: '一简单的VuePress项目部署在21云盒子',
+  themeConfig: {
+    nav: [{ text: 'Home', link: '/' }]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# VuePress在21云盒子
+
+一简单的VuePress项目部署在21云盒子
