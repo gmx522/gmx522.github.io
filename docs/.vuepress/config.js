e5bb309df4ac88746405169ea52820aab5808d8e
20012477 <gaomingxi@haier.com>
2020-07-29 17:51:55 +0800

    初始版本第一个优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,41 +1,40 @@
 module.exports = {
      // 网站标题
-    title: 'VuePress 中文文档 | VuePress 中文网',
+    title: '记录点滴',
     // 网站描述
     description: 'Just playing around', 
-    base: '/',
     logo: '/logo.png',
     // 导航栏
     themeConfig: {
       nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Guide', link: '/guide/' },
-        { text: 'Foo', link: '/foo/' },
-        { text: 'External', link: 'https://baidu.com' },
-        {
-          text: 'Languages',
-          ariaLabel: 'Language Menu',
-          items: [
-            { text: 'Chinese', link: '/language/chinese/' },
-            { text: 'Japanese', link: '/language/japanese/' }
-          ]
-        }
+        { text: '首页', link: '/' },
+        { text: '指南', link: '/guide/' },
+        // { text: 'Foo', link: '/foo/' },
+        // { text: 'External', link: 'https://baidu.com' },
+        // {
+        //   text: 'Languages',
+        //   ariaLabel: 'Language Menu',
+        //   items: [
+        //     { text: 'Chinese', link: '/language/chinese/' },
+        //     { text: 'Japanese', link: '/language/japanese/' }
+        //   ]
+        // }
       ],
       // 最后更新时间
-      lastUpdated: 'Last Updated', // string | boolean
+      lastUpdated: '上次更新', // string | boolean
       // 
       sidebar: 'auto',
       // sidebarDepth: 2,
       // 侧边栏 https://www.vuepress.cn/theme/default-theme-config.html#%E4%BE%A7%E8%BE%B9%E6%A0%8F%E5%88%86%E7%BB%84
-      // 注意：侧边栏的导航是分组的，下面的配置方式是两个组
+      // 注意：侧边栏的导航是分组的，下面的配置方式是两个组，'/guide/' 会对应 前面的分组
       sidebar: {
         '/guide/':[         
             {
-                title: 'Guide',   // 一级菜单名称
+                title: '指南',   // 一级菜单名称
                 collapsable: false, // false为默认展开菜单, 默认值true是折叠,
                 sidebarDepth: 2,    //  设置侧边导航自动提取markdown文件标题的层级，默认1为h2层级
                 children: [
-                    ['', 'guide'],  //菜单名称为'子菜单1'，跳转至/foo/README.md
+                    ['', '介绍'],  //菜单名称为'子菜单1'，跳转至/guide/README.md
                 ]
             },
         ],
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 ---
 home: true
-heroImage: /hero-1.jpg
-heroText: Hero 标题
-tagline: Hero 副标题
-actionText: 快速上手 →
-actionLink: /zh/guide/
+heroImage: /hero.jpg
+heroText: 记录点滴
+# tagline: Hero 副标题
+actionText: 开始 →
+actionLink: /guide/
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
@@ -12,5 +12,5 @@ features:
   details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
 - title: 高性能
   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+footer: MIT Licensed | Copyright © 2020-present Evan You
 ---
