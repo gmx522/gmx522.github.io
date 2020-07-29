741f07a400cd4b84aab89a7360cfe86e4788298d
20012477 <gaomingxi@haier.com>
2020-07-29 14:23:54 +0800

    del

--- docs/.vuepress/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  title: 'VuePress 在21云盒子',
-  description: '一简单的VuePress项目部署在21云盒子',
-  themeConfig: {
-    nav: [{ text: 'Home', link: '/' }]
-  }
-}
--- docs/README.md
+++
@@ -1,3 +0,0 @@
-# VuePress在21云盒子
-
-一简单的VuePress项目部署在21云盒子
